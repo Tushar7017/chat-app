558160e356ed3cdad71d2ce2a3fb504ce0930448
Tushar <Tusharyadav889@gmail.com>
2021-08-08 23:13:16 +0530

    added button to create new chat room

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -1,9 +1,11 @@
 export function getNameInitials(name) {
 
-    const splitName = name.toUpperCase().split(' ');
+    const splitName = name.toUpperCase().split('');
 
     if (splitName.lenght > 1) {
-        return splitName[0][0] + splitName[1][0];
+        return (
+            splitName[0][0] + splitName[1][0]
+        )
     }
     return splitName[0][0];
 }
