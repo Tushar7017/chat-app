2c8cba7552f3f0d6a646e190cd736a594fbe06db
Tushar <Tusharyadav889@gmail.com>
2021-08-15 19:08:15 +0530

    setup cloud functions with nvm

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable indent */
+const functions = require("firebase-functions");
+
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./service-account.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://chat-web-app-fc21c-default-rtdb.firebaseio.com",
+});
